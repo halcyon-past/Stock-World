6f1a910c4a679d8872b33548fb2bc051aece532b
Aritro Saha <70253408+halcyon-past@users.noreply.github.com>
2023-12-25 12:55:47 +0530

    Update App.jsx

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const getStockData = useCallback(async () => {
     try {
       setLoading(false);
-      const res = await axios.get('http://localhost:3000/stocks');
+      const res = await axios.get('https://vivacious-frog-clothes.cyclic.app/stocks');
       setStockData(res.data);
     } catch (error) {
       console.error('Error fetching data:', error);
@@ -23,7 +23,7 @@ function App() {
 
   const loadData = useCallback(async () => {
     try{
-      await axios.get('http://localhost:3000/refresh');
+      await axios.get('https://vivacious-frog-clothes.cyclic.app/refresh');
     }catch (error){
       console.error('Error fetching data:', error);
     }
